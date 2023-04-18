4e5f0d8840ccbafa73b38dfee4d6447aece1cd6e
Yuhwan <point325@gmail.com>
2023-04-18 16:39:02 +0900

    플래그랑 상관없음

--- src/lib/puppeteer.ts
+++ src/lib/puppeteer.ts
@@ -4,14 +4,7 @@ let _browser: Browser;
 
 export async function startScrapingServer(callback) {
   _browser = await puppeteer.launch({
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-web-security",
-      "--disable-features=IsolateOrigins",
-      "--disable-site-isolation-trials",
-      "--disable-features=site-per-process",
-    ],
+    args: ["--no-sandbox", "--disable-web-security"],
     executablePath: process.env.CHROME_PATH,
     headless: true,
   });
