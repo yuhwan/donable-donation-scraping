95bb596a1e442659b07e7a89ab559f70615e61d4
Yuhwan <point325@gmail.com>
2023-04-18 16:51:08 +0900

    플래그 다시 한번 시도

--- src/lib/puppeteer.ts
+++ src/lib/puppeteer.ts
@@ -4,7 +4,14 @@ let _browser: Browser;
 
 export async function startScrapingServer(callback) {
   _browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-web-security"],
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-web-security",
+      "--disable-features=IsolateOrigins",
+      "--disable-site-isolation-trials",
+      "--disable-features=site-per-process",
+    ],
     executablePath: process.env.CHROME_PATH,
     headless: true,
   });
